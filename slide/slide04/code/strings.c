704c4582e2e5a07e936718ab9e89befd7a0b186b
zxplsec <xpzhang.math@whu.edu.cn>
2019-03-09 16:02:13 +0800

    Update

--- slide/slide04/code/strings.c
+++ slide/slide04/code/strings.c
@@ -5,8 +5,8 @@ int main(void)
 {
   printf("*%2s*\n", WORD);
   printf("*%15s*\n", WORD);
-  printf("*%15.5s*\n", WORD);
+  printf("*%4.5s*\n", WORD);
   printf("*%-15.5s*\n",	WORD);
-  printf("*%.13s*\n",	WORD);
+  printf("*%.5s*\n",	WORD);
   return 0;
 }
